0cde51e4ea6042ae9753e46afaf892fdb16fd83c
IbRuslan <ribragimov2003@gmail.com>
2023-12-06 18:16:33 +0500

    add search

--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -1,13 +1,34 @@
 import tracksList from './../../assets/tracksList.js'
+import {Track} from "../../components/Track/Track.jsx";
+import {Input} from '@mui/material'
 
 import s from './mainPage.module.scss'
-import {Track} from "../../components/Track/Track.jsx";
+import {useState} from "react";
+
+const runSearch = (query) => {
+    if(query.trim() === '') {
+        return tracksList
+    }
+
+    const loverCaseQuery = query.toLowerCase()
+
+    return tracksList.filter(track => track.title.toLowerCase().includes(loverCaseQuery) || track.artists.toLowerCase().includes(loverCaseQuery))
+}
 
 export const MainPage = () => {
+
+    const [tracks, setTracks] = useState(tracksList)
+
+    const onChangeHandler = (e) => {
+        const result = runSearch(e.target.value)
+        setTracks(result)
+    }
+
     return (
         <div className={s.search}>
+            <Input className={s.input} placeholder={'Поиск треков'} onChange={onChangeHandler} />
             <div className={s.list}>
-                {tracksList.map((track) => <Track key={track.id} {...track}/>)}
+                {tracks.map(track => <Track key={track.id} {...track}/>)}
             </div>
         </div>
     );
