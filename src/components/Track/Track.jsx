ecbce0b448d10f3d7571bfbd0665024b3079bd97
IbRuslan <ribragimov2003@gmail.com>
2023-12-06 18:35:24 +0500

    add audio

--- src/components/Track/Track.jsx
+++ src/components/Track/Track.jsx
@@ -1,17 +1,21 @@
+import {useContext} from "react";
+import {AudioContext} from "../../context/AudioContext.jsx";
 import {IconButton} from '@mui/material'
 import {PlayArrow} from '@mui/icons-material'
-import secondsToMMSS from "../../pages/main-page/secondsToMMSS.js";
+import secondsToMMSS from "../../utils/secondsToMMSS.js";
 
 import s from './track.module.scss'
 
 export const Track = (track) => {
     const {id, src, preview, title, artists, duration} = track
 
+    const {toggleAutoHandler} = useContext(AudioContext)
+
     const formattedDuration = secondsToMMSS(duration)
 
     return (
         <div className={s.track}>
-            <IconButton>
+            <IconButton onClick={() => toggleAutoHandler(track)}>
                 <PlayArrow/>
             </IconButton>
             <img className={s.preview} src={preview} alt="preview"/>
---
+++ src/context/AudioContext.jsx
@@ -0,0 +1,37 @@
+import {createContext, useState} from "react";
+import tracksList from '../assets/tracksList.js'
+
+
+const audio = new Audio()
+export const AudioContext = createContext({})
+
+export const AudioProvider = ({children}) => {
+    const [current, setCurrent] = useState(tracksList[0])
+    const [isPlaying, setIsPlaying] = useState(false)
+
+    const toggleAutoHandler = (track) => {
+        if(current.id !== track.id) {
+            setCurrent(track)
+            setIsPlaying(true)
+
+            audio.src = track.src
+            audio.currentTime = 0
+            audio.play()
+
+            return
+        }
+
+        if(isPlaying) {
+            audio.pause()
+            setIsPlaying(false)
+        } else {
+            audio.play()
+            setIsPlaying(true)
+        }
+    }
+
+    const value = {current, isPlaying, toggleAutoHandler}
+
+    return <AudioContext.Provider value={value}>{children}</AudioContext.Provider>
+
+}
